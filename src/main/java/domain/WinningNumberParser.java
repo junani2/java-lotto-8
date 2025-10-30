3b068490736cb09482f12b73af7cb00af43cd765
윤준환 <yunjunhwan2@gmail.com>
2025-10-31 00:28:20 +0900

    feat:String InputWinningNumber to List

---
+++ src/main/java/domain/WinningNumberParser.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinningNumberParser {
+
+    public List<Integer> parse(String Number){
+        //문자열을 숫자 리스트로 반환
+        List<Integer> winningNumbers = Arrays.stream(Number.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        return winningNumbers;
+    }
+}
