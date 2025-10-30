01415e8627a19f5b9e0322e6b98f0e3c195e036d
윤준환 <yunjunhwan2@gmail.com>
2025-10-31 00:42:53 +0900

    feat:addBonusNumber organize

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,12 @@
 package lotto;
 
+import exception.ErrorMessage;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private Integer bonusNumber;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,6 +17,24 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_OUT_OF_RANGE.getMessage());
+        }
+
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+    }
+    public void addBonusNumber(int bonus){
+
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException(ErrorMessage.NUMBER_OUT_OF_RANGE.getMessage());
+        }
+        if (numbers.contains(bonus)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+        this.bonusNumber = bonus;
     }
 
     // TODO: 추가 기능 구현
