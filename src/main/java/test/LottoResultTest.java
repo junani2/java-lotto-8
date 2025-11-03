52296b4748619c255ecb3dff64c927692315b844
윤준환 <yunjunhwan2@gmail.com>
2025-11-03 20:29:17 +0900

    fix:.

--- src/main/java/test/LottoResultTest.java
+++ src/main/java/test/LottoResultTest.java
@@ -18,15 +18,15 @@ public class LottoResultTest {
         WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", 10);
 
         //6개 일치
-        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto1 = new Lotto(List.of(1,2,3,4,5,6));
         //5개 일치+보너스 일치
-        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 10));
+        Lotto lotto2 = new Lotto(List.of(1,2,3,4,5,10));
         // 5개만 일치
-        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 11));
+        Lotto lotto3 = new Lotto(List.of(1,2,3,4,5,11));
         // 4개만 일치
-        Lotto lotto4 = new Lotto(List.of(3, 4, 5, 6, 8, 9));
+        Lotto lotto4 = new Lotto(List.of(3,4,5,6,8,9));
         // 3개만 일치
-        Lotto lotto5 = new Lotto(List.of(1, 2, 3, 11, 12, 13));
+        Lotto lotto5 = new Lotto(List.of(1,2,3,11,12,13));
 
         List<Lotto> lottos = List.of(lotto1, lotto2, lotto3, lotto4, lotto5);
 
