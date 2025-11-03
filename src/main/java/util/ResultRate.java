2cff887a479172500f12612f8648d95fcff62808
윤준환 <yunjunhwan2@gmail.com>
2025-11-03 13:52:01 +0900

    fix: unnecessary code delete

--- src/main/java/util/ResultRate.java
+++ src/main/java/util/ResultRate.java
@@ -5,8 +5,6 @@ import prize.Prize;
 import java.util.Map;
 
 public class ResultRate {
-    private static final int[] PRIZES = {0, 0, 5000, 50000, 1500000, 30000000, 2000000000};
-
     public static double earningRate(Map<Prize, Integer> result, int amount) {
         int totalRate = result.getOrDefault(Prize.SIX, 0) * Prize.SIX.getMoney()
                 + result.getOrDefault(Prize.FIVE_BONUS, 0) * Prize.FIVE_BONUS.getMoney()
